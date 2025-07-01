75348b266c0eb697b86f4ba19e87168a96574da7
guilhermealves54 <guilherme.alves.mo@gmail.com>
2025-07-01 15:47:31 +0100

    working on ex02

--- Module_00/ex02/Account.cpp
+++ Module_00/ex02/Account.cpp
@@ -6,10 +6,13 @@
 /*   By: gribeiro <gribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 14:32:47 by gribeiro          #+#    #+#             */
-/*   Updated: 2025/07/01 14:42:04 by gribeiro         ###   ########.fr       */
+/*   Updated: 2025/07/01 15:37:35 by gribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <ctime>
+#include <iomanip>
+#include <iostream>
 #include "Account.hpp"
 
 //  CONSTRUCTOR AND DESTRUCTOR
@@ -75,5 +78,17 @@ void	Account::displayStatus( void ) const
 
 void	Account::_displayTimestamp( void )
 {
+    std::time_t relog = std::time(NULL);
+    std::tm *lt = std::localtime(&relog);
 
+    std::cout 
+        << "["
+        << std::setfill('0') << std::setw(4) << lt->tm_year + 1900
+        << std::setw(2) << lt->tm_mon + 1
+        << std::setw(2) << lt->tm_mday 
+        << "_"
+        << std::setw(2) << lt->tm_hour
+        << std::setw(2) << lt->tm_min
+        << std::setw(2) << lt->tm_sec
+        << "] ";
 }
