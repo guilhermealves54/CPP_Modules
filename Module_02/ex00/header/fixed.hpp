60138aa43c61657d7b852c44ac78aff2d434afb8
Guilherme <guilherme.alves.mo@gmail.com>
2025-08-10 23:39:14 +0100

    working M01 ex05

---
+++ Module_02/ex00/header/fixed.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gribeiro <gribeiro@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/08 17:27:07 by gribeiro          #+#    #+#             */
+/*   Updated: 2025/08/08 17:30:06 by gribeiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class	Fixed
+{
+	public:
+		Fixed();
+		~Fixed();
+		Fixed(const Fixed &);
+		Fixed &operator=(const Fixed &);
+		
+	private:
+	
+};
+
+#endif
