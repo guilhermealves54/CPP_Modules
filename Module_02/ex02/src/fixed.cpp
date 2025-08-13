330c4fcc74b6fb61be8c4b5c8a0d7da305734d22
guilhermealves54 <guilherme.alves.mo@gmail.com>
2025-08-13 16:10:31 +0100

    M02 OK.

--- Module_02/ex02/src/fixed.cpp
+++ Module_02/ex02/src/fixed.cpp
@@ -6,44 +6,33 @@
 /*   By: gribeiro <gribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 15:15:37 by gribeiro          #+#    #+#             */
-/*   Updated: 2025/08/13 15:25:39 by gribeiro         ###   ########.fr       */
+/*   Updated: 2025/08/13 16:09:48 by gribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/fixed.hpp"
 
-Fixed::Fixed()
-{
-	std::cout << "Default constructor called" << std::endl;
-	_value = 0;
-}
+Fixed::Fixed() : _value(0) {}
 
-Fixed::~Fixed()
-{
-	std::cout << "Destructor called" << std::endl;
-}
+Fixed::~Fixed() {}
 
 Fixed::Fixed(const int val)
 {
-	std::cout << "Int constructor called" << std::endl;
 	_value = val << _fractionalBits;
 }
 
 Fixed::Fixed(const float val)
 {
-	std::cout << "Float constructor called" << std::endl;
 	_value = roundf(val * (1 << _fractionalBits));
 }
 
 Fixed::Fixed(const Fixed &other)
 {
-	std::cout << "Copy constructor called" << std::endl;
 	*this = other;
 }
 
 Fixed &Fixed::operator=(const Fixed &other)
 {
-	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other)
 		this->_value = other.getRawBits();
 	return *this;
@@ -51,13 +40,11 @@ Fixed &Fixed::operator=(const Fixed &other)
 
 int Fixed::getRawBits() const
 {
-	std::cout << "getRawBits member function called" << std::endl;
 	return this->_value;
 }
 
 void Fixed::setRawBits(int const raw)
 {
-	std::cout << "setRawBits member function called" << std::endl;
 	this->_value = raw;
 }
 
@@ -124,22 +111,34 @@ bool Fixed::operator!=(Fixed const &other) const
 //	Arithmetic operators overload
 Fixed Fixed::operator+(Fixed const &other) const
 {
-	return (_value + other._value);
+	Fixed result;
+
+	result.setRawBits(_value + other._value);
+	return (result);
 }
 
 Fixed Fixed::operator-(Fixed const &other) const
 {
-	return (_value - other._value);
+	Fixed result;
+
+	result.setRawBits(_value - other._value);
+	return (result);
 }
 
 Fixed Fixed::operator*(Fixed const &other) const
 {
-	return (_value * other._value);
+	Fixed result;
+	
+	result.setRawBits((_value * other._value) >> _fractionalBits);
+	return (result);
 }
 
 Fixed Fixed::operator/(Fixed const &other) const
 {
-	return (_value / other._value);
+	Fixed result;
+
+	result.setRawBits((_value / other._value) << _fractionalBits);
+	return (result);
 }
 
 //	Increment/decrement operators overload
@@ -173,22 +172,26 @@ Fixed Fixed::operator--(int)
 
 
 //	Static min and max
-static Fixed &Fixed::min(Fixed &a, Fixed &b)
+Fixed &Fixed::min(Fixed &a, Fixed &b)
 {
-
+	return (a < b ? a : b);
 }
 
-static const Fixed &Fixed::min(Fixed const &a, Fixed const &b)
+const Fixed &Fixed::min(Fixed const &a, Fixed const &b)
 {
-
+	if (a < b)
+		return (a);
+	return (b);
 }
 
-static Fixed &Fixed::max(Fixed &a, Fixed &b)
+Fixed &Fixed::max(Fixed &a, Fixed &b)
 {
-
+	return (a > b ? a : b);
 }
 
-static const Fixed &Fixed::max(Fixed const &a, Fixed const &b)
+const Fixed &Fixed::max(Fixed const &a, Fixed const &b)
 {
-	
+	if (a > b)
+		return (a);
+	return (b);
 }
--- Module_02/ex02/src/main.cpp
+++ Module_02/ex02/src/main.cpp
@@ -6,15 +6,24 @@
 /*   By: gribeiro <gribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 15:42:11 by gribeiro          #+#    #+#             */
-/*   Updated: 2025/08/13 14:38:32 by gribeiro         ###   ########.fr       */
+/*   Updated: 2025/08/13 15:58:57 by gribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/fixed.hpp"
 
-int main(void)
+int main( void ) 
 {
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
 
 	return 0;
 }
